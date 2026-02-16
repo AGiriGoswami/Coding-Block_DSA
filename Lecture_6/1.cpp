a450288fd6e3b0707c8865a3031e4ed591e0f709
AGiriGoswami <ag124767@gmail.com>
2026-02-16 10:43:39 +0530

    a

--- Lecture_6/1.cpp
+++ Lecture_6/1.cpp
@@ -2,6 +2,22 @@
 // n = 5
 // * * * * *
 
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cout << "Enter the number of rows: ";
+//     cin >> n;
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         cout << " * ";
+//     }
+
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 int main()
@@ -12,8 +28,10 @@ int main()
 
     for (int i = 1; i <= n; i++)
     {
-        cout << " * ";
+        for (int j = 1; j <= n; j++){
+            cout << " * ";
+        }
+        cout << endl;
     }
-
-    return 0;
-}
+        return 0;
+    }
