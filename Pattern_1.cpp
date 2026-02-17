e8810f7dcd63085186d609bc3d6f87f42fcbf6a0
AGiriGoswami <ag124767@gmail.com>
2026-02-17 10:38:15 +0530

    Notes

---
+++ Pattern_1.cpp
@@ -0,0 +1,24 @@
+// * * * * *
+//     * * * *
+//        * * *
+//            * *
+//               *
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the Rows:";
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        // Space Printing
+        for(int j=1; j<=2*i-2; j++){
+            cout << "  ";
+        }
+        // Star Printing
+        for(int j=1; j<=n-i+1; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
