4d8b8c3adb7ef91858a67001603b2546be4c57cc
AGiriGoswami <ag124767@gmail.com>
2026-02-18 11:49:12 +0530

    Notes

---
+++ Lecture_8/Patter_5.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 7;
+    int m = (n+1)/2;
+  
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=m-i;j++){
+            cout<<"  ";
+        }
+        if(i==1){
+            cout<<"* ";
+        }
+        else{
+            cout<<"* ";
+            for(int j=1;j<=2*i-3;j++){
+                cout<<"  ";
+            }
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+
+    // 
+
+      for(int i=m-1;i>=1;i--){
+        for(int j=1;j<=m-i;j++){
+            cout<<"  ";
+        }
+        if(i==1){
+            cout<<"* ";
+        }
+        else{
+            cout<<"* ";
+            for(int j=1;j<=2*i-3;j++){
+                cout<<"  ";
+            }
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
