eb9a2934a6aefa250eebf5a1346cabe1d45b5c88
AGiriGoswami <ag124767@gmail.com>
2026-02-19 10:37:55 +0530

    Notes

--- Lecture_8/Patter_5.cpp
+++ Lecture_8/Patter_5.cpp
@@ -16,24 +16,28 @@ int main(){
   
     for(int i=1;i<=m;i++){
         for(int j=1;j<=m-i;j++){
-            // 
+            // space printing
             cout<<"  ";
         }
+        // star printing
         if(i==1){
             cout<<"* ";
         }
         else{
+            // first star
             cout<<"* ";
             for(int j=1;j<=2*i-3;j++){
+                // space printing
                 cout<<"  ";
             }
+            // second star
             cout<<"* ";
         }
         cout<<endl;
     }
 
 
-    // 
+    // Rerverse Pattern
 
       for(int i=m-1;i>=1;i--){
         for(int j=1;j<=m-i;j++){
