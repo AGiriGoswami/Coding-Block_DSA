f938c6665675352aad98039c45e8d590b36491f9
AGiriGoswami <ag124767@gmail.com>
2026-02-19 11:13:56 +0530

    Notes

---
+++ Lecture_9/Pattern_1.cpp
@@ -0,0 +1,37 @@
+
+// *               * 
+// * *           * * 
+// * * *       * * * 
+// * * * *   * * * * 
+// * * * * * * * * * 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    for (int i =1; i<=n;i++){
+        for (int j=1; j<=n; j++){
+            if (j<=i){
+                cout << "* ";
+            }
+            else{
+                cout << "  ";
+            }
+        }
+        for (int j=n-1; j>=1; j--){
+            if (j<=i){
+                cout << "* ";
+            }
+            else{
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+   
+}
