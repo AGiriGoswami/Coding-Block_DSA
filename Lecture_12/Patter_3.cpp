afd9a9581a617ea6ecfc0300f97c1ce50ba9cc81
AGiriGoswami <ag124767@gmail.com>
2026-02-24 11:31:56 +0530

    Notes

--- Lecture_12/Patter_3.cpp
+++ Lecture_12/Patter_3.cpp
@@ -1,9 +1,49 @@
+
+// * * *     * * * 
+// * *         * *
+// *             *
+
+// *             *
+// * *         * *
+// * * *     * * *
+
 #include<iostream>
 using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i=1; i<=n;i++){
-        
+    int m = (n+1)/2;
+    // Upper half
+    for(int i=1; i<=m;i++){
+        // left star
+        for(int j=1;j<=m-i;j++){
+            cout<<"* ";
+        }
+        // space
+        for(int j=1;j<=2*i;j++){
+            cout<<"  ";
+        }
+        // right star
+        for(int j=1;j<=m-i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
     }
+    // Lower half
+    for(int i=m-1;i>=1;i--){
+        // left star
+        for(int j=1;j<=m-i;j++){
+            cout<<"* ";
+        }
+        // space
+        for(int j=1;j<=2*i;j++){
+            cout<<"  ";
+        }
+        // right star
+        for(int j=1;j<=m-i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
