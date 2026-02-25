16741e0d1632dab9f54d7a1d8a98463b4f81a3ae
AGiriGoswami <ag124767@gmail.com>
2026-02-25 11:16:59 +0530

    Notes

--- Lecture_13/Pattern_2.cpp
+++ Lecture_13/Pattern_2.cpp
@@ -1,3 +1,15 @@
+
+//         * * * * * 
+//       * * * * 
+//     * * * 
+//   * * 
+// * 
+//   * * 
+//     * * * 
+//       * * * * 
+//         * * * * * 
+
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -7,22 +19,24 @@ int main(){
     for(int i=1;i<=n;i++){
         // spaces
         for(int j=1;j<=n-i;j++){
-            cout<<"* ";
-        }
-        // star
-        for(int j=1;j<=2*i-2;j++){
             cout<<"  ";
-        }
-        cout<<endl;
-    }
-        for(int i=n-1;i>=1;i--){
-        // spaces
-        for(int j=1;j<=2*i-2;j++){
-            cout<<"* ";
         }
         // star
         for(int j=1;j<=n-i+1;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+    // Lower
+     for(int i=n-1;i>=1;i--){
+        // spaces
+        for(int j=1;j<=n-i;j++){
             cout<<"  ";
+        }
+        // star
+        for(int j=1;j<=n-i+1;j++){
+            cout<<"* ";
         }
         cout<<endl;
     }
