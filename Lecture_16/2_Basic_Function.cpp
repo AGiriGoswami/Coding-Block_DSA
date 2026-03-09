776bc755acebcde06fb8fc07e5525c2e6dcf8366
AGiriGoswami <ag124767@gmail.com>
2026-03-09 10:52:55 +0530

    notes

--- Lecture_16/2_Basic_Function.cpp
+++ Lecture_16/2_Basic_Function.cpp
@@ -1,5 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    
+
+// No Arguement and No return
+
+void greet()
+{
+    cout << "Good Morning" << endl;
+}
+
+// No Arguement and return
+
+int subtract()
+{
+    int a = 5;
+    int b = 2;
+    int c = a - b;
+    return c;
+}
+
+// Arguement and No return
+
+void hello(string name)
+{
+    cout << "Hello " << name << endl;
+}
+
+// Arguement and return
+
+int add(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
+
+int main()
+{
+
+    greet();
+    cout << subtract() << endl;
+    hello("ajay");
+    cout << add(10, 30) << endl;
+
+    return 0;
 }
