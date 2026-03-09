ac312e864e4e4ca011281458cda3b023f7256457
AGiriGoswami <ag124767@gmail.com>
2026-03-09 10:26:46 +0530

    Notes

---
+++ Lecture_16/1_Datatypes.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 3;
+    float b =2.00;
+    double c = 3.5480;
+    char d = 'a';
+    bool e = true;
+    string s = "Ajay";
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+    cout<<s<<endl;
+
+    // find the datatype size
+
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(b)<<endl;
+    cout<<sizeof(c)<<endl;
+    cout<<sizeof(d)<<endl;
+    cout<<sizeof(e)<<endl;
+    cout<<sizeof(f)<<endl;
+    cout<<sizeof(g)<<endl;
+}
