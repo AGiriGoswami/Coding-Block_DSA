712a6e83196b10aaac750771442833ac0e66a077
AGiriGoswami <ag124767@gmail.com>
2026-03-09 10:27:56 +0530

    Notes

--- Lecture_16/1_Datatypes.cpp
+++ Lecture_16/1_Datatypes.cpp
@@ -18,11 +18,12 @@ int main(){
 
     // find the datatype size
 
-    cout<<sizeof(a)<<endl;
+    cout<< "Size of int" <<sizeof(a)<<endl;
     cout<<sizeof(b)<<endl;
     cout<<sizeof(c)<<endl;
     cout<<sizeof(d)<<endl;
     cout<<sizeof(e)<<endl;
-    cout<<sizeof(f)<<endl;
-    cout<<sizeof(g)<<endl;
+    cout<<sizeof(s)<<endl;
+
+    return 0;
 }
