991b3af95905eaf12235c6475f198a949ca6621a
AGiriGoswami <ag124767@gmail.com>
2026-03-12 10:26:31 +0530

    Notes

---
+++ Lecture_17/Prime_Number.cpp
@@ -0,0 +1,16 @@
+// Prime Number
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for (int i = 2;i<=n-i;i++){
+        if(n%i == 0){
+            cout<<" Not Prime";
+            return 0;
+        }
+    }
+    cout<<"Prime Number";
+
+}
