a87e792ea1839b207a254182b2cf971d42760c91
AGiriGoswami <ag124767@gmail.com>
2026-03-18 11:04:08 +0530

    Notes

--- Lecture_20/Vector.cpp
+++
@@ -1,8 +0,0 @@
-// vector 
-// Create Data Type
-
-// vector <int> ans
-// vector is size ka kitne element stronge hai
-// vector me capcity kitne element show kare ha hai
-
-
---
+++ Lecture_23/Vector.cpp
@@ -0,0 +1,64 @@
+// vector 
+// Create Data Type
+
+// vector <int> ans
+// vector is size ka kitne element stronge hai
+// vector me capcity kitne element show kare ha hai
+
+
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    // create vector with defined size
+    vector<int> a(5);
+
+    // create vector with defined number
+    vector<int> b = {1,2,3,4,5};
+
+    // create vector with user input size
+    vector<int> c;
+    // int n;
+    // cin>>n;
+    // vector<int> d(n);
+
+    c.push_back(1);
+    c.push_back(2);
+    c.push_back(3);
+    c.push_back(4);
+    c.push_back(5);
+    c.push_back(6);
+    c.push_back(7);
+    c.push_back(8);
+    c.push_back(9);
+    c.push_back(10);
+
+    for(int i = 0; i<c.size();i++){
+        cout<<c[i]<<" ";
+    }
+
+    cout<<endl;
+    cout<<c.size()<<endl;
+    cout<<c.capacity()<<endl;
+
+    c.pop_back();
+    c.pop_back();
+    c.pop_back();
+
+    cout<<"After "<<endl;
+    for(int i = 0; i<c.size();i++){
+        cout<<c[i]<<" ";
+    }
+
+    cout<<endl;
+    cout<<c.size()<<endl;
+    cout<<c.capacity()<<endl;
+
+    
+    return 0;
+
+}
