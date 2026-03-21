c4d79df3c8ddb12f548c93a91684a8334764e04b
AGiriGoswami <ag124767@gmail.com>
2026-03-21 21:38:48 +0530

    Notes

---
+++ Basic/Code.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n = 5;
+    // cin>>n;
+
+    // * * * * *
+    // * * * * *
+    // * * * * *
+    // * * * * *
+    // * * * * *
+    // for(int i =0; i<n;i++){
+    //     for(int j = 0; j < n ; j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 1 2 3 4 5
+    // 1 2 3 4 5
+    // 1 2 3 4 5
+    // 1 2 3 4 5
+    // 1 2 3 4 5
+
+    // for(int i =1; i<=n; i++){
+    //     for(int j =1; j<=n; j++){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // A B C D E
+    // A B C D E
+    // A B C D E
+    // A B C D E
+    // A B C D E
+
+    // for(int i =0; i<n;i++){
+    //     char a = 'A';
+    //     for(int j =0; j<n; j++){
+    //         cout<<a<<" ";
+    //         a ++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 5 4 3 2 1
+    // 5 4 3 2 1
+    // 5 4 3 2 1
+    // 5 4 3 2 1
+    // 5 4 3 2 1
+
+    // for(int i =n; i>=1 ; i--){
+    //     for(int j = n; j>=1; j--){
+    //         cout<< j <<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 1
+    // 1 2
+    // 1 2 3
+    // 1 2 3 4
+    // 1 2 3 4 5
+
+    // for(int i = 1; i<=n ; i++){
+    //     for(int j =1; j<=i; j++){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 5
+    // 5 4
+    // 5 4 3
+    // 5 4 3 2
+    // 5 4 3 2 1
+
+    // for(int i = n ; i>=1; i--){
+    //     for(int j = n ; j>=i; j--){
+    //         cout<< j<< " ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // A
+    // A B
+    // A B C
+    // A B C D
+    // A B C D E
+
+    //  for(int i = n ; i>=1; i--){
+    //     char a = 'A';
+    //     for(int j = n ; j>=i; j--){
+    //         cout<< a<< " ";
+    //         a++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 1 2 3 4 5
+    // 1 2 3 4
+    // 1 2 3
+    // 1 2
+    // 1
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    // A B C D E
+    // A B C D
+    // A B C
+    // A B
+    // A
+
+    for (int i = 1; i <= n; i++)
+    {
+        char a = 'A';
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << a << " ";
+            a++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
