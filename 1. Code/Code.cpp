ebe8067c32f74a09b0d5a3579ae9c41494c851ce
AGiriGoswami <ag124767@gmail.com>
2026-03-22 23:11:17 +0530

    notes

--- 1. Code/Code.cpp
+++ 1. Code/Code.cpp
@@ -447,6 +447,24 @@ int main()
 
     for(int i =0; i<n ; i++){
 
+        // spaceing
+        for(int j =0 ; j<n-i-1; j++){
+            cout<<" ";
+        }
+        cout<<"*";
+
+        if(i!=0){
+            // spaces
+            for(int j = 0; j<2*i-1; j++){
+                cout<<" ";
+            }
+            star
+            cout<<"* ";
+
+        }
+
+        cout<<endl;
+
     }
 
 
