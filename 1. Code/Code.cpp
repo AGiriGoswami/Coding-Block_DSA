0e6d1b7bf75ba3049ec410e11d67d9b84e6f78c1
AGiriGoswami <ag124767@gmail.com>
2026-03-22 21:56:54 +0530

    Notes

--- 1. Code/Code.cpp
+++ 1. Code/Code.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
 
-    int n = 5;
-    cout << "Enter the Number in Rows: ";
-    cin >> n;
+    // int n = 5;
+    // cout << "Enter the Number in Rows: ";
+    // cin >> n;
 
     // * * * * *
     // * * * * *
@@ -266,14 +266,74 @@ int main()
     // }
     // cout << sum;
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j = i + 1; j++)
-        {
-            cout << j;
-        }
-        cout << endl;
-    }
+    // 1
+    // 2 2
+    // 3 3 3
+    // 4 4 4 4
+    // int n = 4;
+
+    // for(int i = 0; i < n; i++) {
+    //     for(int j = 0; j < i + 1; j++) {
+    //         cout << (i+1) <<" ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // A
+    // A B
+    // A B C
+    // A B C D
+
+    //     int n =4;
+    // for(int i = 1; i <= n; i++) {
+    //         char a = 'A';
+
+    //         for(int j = 1; j <= i; j++) {
+    //             cout << a << " ";
+    //             a++;
+    //         }
+
+    //         cout << endl;
+    //     }
+
+    // int n = 5;
+    // for(int i=1;i<=n;i++){
+    //     char a= 'a';
+    //     for(int j=1; j<= n-i+1; j++){
+    //         cout<<"* " << a << " ";
+    //         a++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int n = 4;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i + 1; j > 0; j--)
+    //     {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+// A 
+// B C
+// D E F
+// G H I J
+
+    // int n = 4;
+    // int sum = 1;
+    // char a = 'A';
+    // for(int i =0; i<n; i++){
+    //     for(int j = i+1; j>0 ; j--){
+    //         cout<<a<<" ";
+    //         a++;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    
 
     return 0;
 }
