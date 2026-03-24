0fb5c440b661b40e586e4d5defaaab7a9923c65f
AGiriGoswami <ag124767@gmail.com>
2026-03-24 23:11:27 +0530

    Notes

--- 1. Code/Code.cpp
+++ 1. Code/Code.cpp
@@ -466,23 +466,25 @@ int main()
 //    1 2  * * * * *
 //    1  * * * * * * *
 
-    int n = 5;
+    // int n = 5;
 
-    for(int i =1 ; i<=n ; i++){
-        for(int j =1; j<n-1;j++){
-            cout<<" ";
-        }
-        for(int j =1; j<=n-i+1; j++){
-            cout<<j<<" ";
-        }
-        for(int j =2; j<=i; j++){
-            cout<<" *";
-        }
-        for(int j =i-1; j>1; j--){
-            cout<<" *";
-        }
-        cout<<endl;
-    }
+    // for(int i =1 ; i<=n ; i++){
+    //     for(int j =1; j<n-1;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j =1; j<=n-i+1; j++){
+    //         cout<<j<<" ";
+    //     }
+    //     for(int j =2; j<=i; j++){
+    //         cout<<" *";
+    //     }
+    //     for(int j =i-1; j>1; j--){
+    //         cout<<" *";
+    //     }
+    //     cout<<endl;
+    // }
+
+    
 
     return 0;
 }
